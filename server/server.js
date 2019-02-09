b7c35d1119fa553a8428a82fd1e56bc738dc6059
oldmo <105306006@nccu.edu.tw>
2019-02-09 10:33:38 +0800

    :id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const { ObjectID } = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -27,6 +27,33 @@ app.get('/todos',(req,res) => {
     res.send({todos});
   },(err) => res.status(400).send(err))
 })
+
+app.get('/todos/:id',(req,res) => {
+  var id = req.params.id;
+
+  //valid id via isvalid
+    //404 -send back empty send
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  //findId
+    //success
+      //if todos - send it back
+      //if no todos - send back 404 with empty body
+    //error
+      //400 - and send empty body back
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }else{
+      return res.send({todo: todo})
+    }
+  }).catch((e) => {
+    res.status(400).send();
+  })
+})
+
 
 app.listen(3000, () => {
   console.log(`starting on port 3000`);
